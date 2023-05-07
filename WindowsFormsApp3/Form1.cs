a3f4f1c07e6f741d0baf614e2f2e1bbce87e5913
nawars <nonaibrahim585@gmail.come>
2023-05-07 19:38:09 +0300

    Add project files.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+private void Button1_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt16(textBox1.Text);
+            int y = 1;
+            for(int i=0;i<x;i++)
+            {
+                y = y * i;
+
+            }
+        }
+    } }
+  
+
